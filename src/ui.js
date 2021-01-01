2bd75bad594d46c99a7aa3cd7b42bb3de636115e
Aung Myo Kyaw <aungmyokyaw.git@icloud.com>
2021-01-01 23:06:03 +0630

    feat: namecard component

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,5 @@
 const React = require('react');
+const PropTypes = require('prop-types');
 const Ink = require('ink');
 const BigText = require('ink-big-text');
 const Link = require('ink-link');
@@ -6,52 +7,59 @@ const Link = require('ink-link');
 const { Component } = React;
 const { render, Color, Box } = Ink;
 
-class AungMyoKyaw extends Component {
+const nameCardConfig = {
+  name: 'Aung Myo Kyaw',
+  about: 'nothing',
+  items: [
+    {
+      label: 'Website',
+      url: 'https://www.aungmyokyaw.com/',
+    },
+    {
+      label: 'Twitter',
+      url: 'https://twitter.com/aung_myo_kyaw',
+    },
+    {
+      label: 'Github',
+      url: 'https://github.com/AungMyoKyaw',
+    },
+    {
+      label: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/aungmyokyaw/',
+    },
+    {
+      label: 'Blog',
+      url: 'https://blog.aungmyokyaw.com/',
+    },
+  ],
+};
+
+class NameCard extends Component {
   constructor() {
     super();
-    this.state = {
-      name: 'Aung Myo Kyaw',
-      about: 'nothing',
-      items: [
-        {
-          label: 'Website',
-          url: 'https://www.aungmyokyaw.com/'
-        },
-        {
-          label: 'Twitter',
-          url: 'https://twitter.com/MgAungMyoKyaw'
-        },
-        {
-          label: 'Github',
-          url: 'https://github.com/AungMyoKyaw'
-        },
-        {
-          label: 'LinkedIn',
-          url: 'https://www.linkedin.com/in/aungmyokyaw/'
-        },
-        {
-          label: 'Blog',
-          url: 'https://blog.aungmyokyaw.com/'
-        }
-      ]
-    };
   }
   render() {
     return (
-      <Box flexDirection="column" padding={1}>
-        <Box marginBottom={1}>
-          <Color green>
-            <BigText
-              text={this.state.name}
-              font="chrome"
-              space={false}
-              align="left"
-            />
-          </Color>
+      <Box
+        borderStyle="double"
+        borderColor="cyan"
+        float="center"
+        flexDirection="column"
+        padding={1}
+      >
+        <Box float="center" marginBottom={1}>
+          <BigText
+            text={this.props.name}
+            font="tiny"
+            space={false}
+            colors={['green']}
+            background="blue"
+            align="left"
+          />
         </Box>
-        {this.state.items.map((item, i) => {
+        {this.props.items.map((item, i) => {
           return (
-            <Box marginLeft={1} marginBottom={1} key={i}>
+            <Box float="center" marginLeft={1} marginBottom={1} key={i}>
               <Color green>
                 <Link url={item.url}>{item.label}</Link>
               </Color>
@@ -63,4 +71,16 @@ class AungMyoKyaw extends Component {
   }
 }
 
-render(<AungMyoKyaw />);
+NameCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  about: PropTypes.string,
+  items: PropTypes.array.isRequired,
+};
+
+render(
+  <NameCard
+    name={nameCardConfig.name}
+    about={nameCardConfig.about}
+    items={nameCardConfig.items}
+  />
+);
